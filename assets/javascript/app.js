4e5fd3c6ace2bd4f13b22df55f09beb8ce1f51e8
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-06-04 17:36:41 -0400

    first slide

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
 var number = 31;
 var intervalId;
+var correctAnswers = 0;
+var wrongAnswers = 0;
 
 
 function run() {
@@ -13,7 +15,6 @@ function decrement() {
 	number--;
 	$("#timer").html("<h2>-" + number + "</h2>");
 	if (number === 0) {
-	answerOneCorrect();
 	stop();
 	}
 }
@@ -40,19 +41,31 @@ function questionOne() {
 	$('.temp').remove();
 	$('#time-bomb').append('<span id="timer"></span>');
 	$('#content').append('<h3 class="temp">What Movie Launched The Marvel Cinematic Universe?</h3>');
-	$('#content').append('<h4 class="temp" id="wrong">THE INCREDIBLE HULK</h4>');
+	$('#content').append('<h4 class="temp" id="wrong1">THE INCREDIBLE HULK</h4>');
 	$('#content').append('<h4 class="temp" id="correct">IRONMAN</h4>');
-	$('#content').append('<h4 class="temp" id="wrong">THOR</h4>');
-	$('#content').append('<h4 class="temp" id="wrong">X-MEN</h4>');
+	$('#content').append('<h4 class="temp" id="wrong2">THOR</h4>');
+	$('#content').append('<h4 class="temp" id="wrong3">X-MEN</h4>');
 
+	$('#wrong1,#wrong2,#wrong3').click(answerOneWrong);
 	$('#correct').click(answerOneCorrect);
+
 
 }
 
 function answerOneCorrect() {
+	correctAnswers++;
 	$('.temp').remove();
 	stop();
 	$('#content').append('<h3 class="temp">CORRECT! IRONMAN</h3>');
+	$('#content').append('<iframe src="https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="366" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+
+}
+
+function answerOneWrong() {
+	wrongAnswers++;
+	$('.temp').remove();
+	stop();
+	$('#content').append('<h3 class="temp">WRONG! IRONMAN</h3>');
 	$('#content').append('<iframe src="https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="366" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
 
 }
