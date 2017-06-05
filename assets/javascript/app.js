759c827a8b5d00f5c84e683a387bfdbe02646526
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-06-05 19:13:28 -0400

    css fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,96 +1,142 @@
 $(document).ready(function() {
 
 
-var number = 31;
-var intervalId;
+var questions = ['What Movie Launched The Marvel Cinematic Universe?', 'What is the Name of Thor&rsquo;s Brother?', 'Who is the First Actor to Play Spiderman in the MCU?', 'What is Peter Quill&rsquo;s Self-Appointed Nickname?', 'What Was Ant-Man&rsquo;s Previous Career?', 'How Many Infinity Stones are in the MCU?', 'What is the Winter Soldier&rsquo;s Name?', 'What are the Only Words Groot Said Besides <q>I Am Groot</q>?', 'What Actor Played Doctor Strange?', 'What Phase is the MCU Currently In?'];
+var answerArray = [['THE INCREDIBLE HULK', 'IRONMAN', 'THOR', 'X-MEN'], ['ZEUS', 'APOLLO', 'JEFF', 'LOKI'], ['TOM HOLLAND', 'ANDREW GARFIELD', 'TOBEY MAGUIRE', 'NEIL PATRICK HARRIS'], ['MOON-GOD', 'SPACE-JESUS', 'STAR-LORD', 'LORD-BYRON'], ['CON-ARTIST', 'JEWELER', 'PLUMBER', 'BURGLAR'], ['SIX', 'THREE', 'FIVE', 'TEN'], ['FRANK WATTS', 'BUCKY BARNES', 'FATTY ARBUCKLE', 'NICKY BARNES'], ['WE ARE GROOT', 'I AM HUNGRY', 'I LOVE YOU', 'I LIKE TURTLES'], ['BUTTERCUP PUMPKINPATCH', 'BUFFALO CUSTARDBATH', 'BENEDICT CUMBERBATCH', 'BENEDRYL CUMBERSNATCH'], ['PHASE ONE', 'PHASE TWO', 'PHASE THREE', 'PHASE FOUR']];
+var correctArray = ['IRONMAN', 'LOKI', 'TOM HOLLAND', 'STAR-LORD', 'BURGLAR', 'SIX', 'BUCKY BARNES', 'WE ARE GROOT', 'BENEDICT CUMBERBATCH', 'PHASE THREE'];
+var gifArray = ['"https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="300" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/KiaAwaemKWhVu" width="480" height="259" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/I2LasqroRY8Mg" width="480" height="270" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/QgCwRigEAxqGQ" width="480" height="294" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/h0gzUb0Wh1RIY" width="480" height="300" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/aqMY57vLdkghi" width="480" height="198" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/oyyXEHFEZPLCo" width="480" height="274" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/F9hQLAVhWnL56" width="480" height="300" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/dvWvnRdc4h440" width="480" height="270" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/RjSRwCJRhRB9S" width="480" height="300" frameBorder="0" class="giphy-embed"'];
+
+var totalScore;
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var timedOut = 0;
+var answer;
+var i = 0;
+
+var counter = 30;
+var intervalId;
+
+// Displays Start Screen
+startScreen();
+
+// Begins When User Presses 'START GAME'
+$('#start-game').click(function(event) {
+	questionDisplay();
+	countdown();
+});
+
+// Brings User Back to the Start Screen for New Game
+$('#reset-game').click(function(event) {
+	resetGame();
+});
+
+// Checks if User Guess is Correct
+$("body").on("click", ".answer", function(event){
+    answer = $(this).text();
+    if(answer === correctArray[i]) {
+        
+        clearInterval(intervalId);
+        correctDisplay();
+    } else {  
+        clearInterval(intervalId);
+        wrongDisplay();
+    }
+}); 
 
 
-function run() {
-	intervalId = setInterval(decrement, 1000);
+// Start Screen of Game
+function startScreen() {
+	$('#content').html('');
+	$('#content').append('<h3>Play The Marvel Cinematic Universe (MCU) Trivia Game!</h3>');
+	$('#content').append('<h5>There Are 10 Questions in the Game.</h5>');
+	$('#content').append('<h5>You Will Have 30 Seconds to Answer Each Question.</h5>');
+	$('#content').append('<h5>Good Luck!</h5>');
+	$('#content').append('<h4 id="start-game">START GAME</h4>');
 }
 
-function decrement() {
-	number--;
-	$("#timer").html("<h2>-" + number + "</h2>");
-	if (number === 0) {
-	stop();
-	}
+// End Screen of Game
+function endScreen() {
+	$('#content').html('');
+	$('#content').append('<h3>GAME OVER</h3>');
+	$('#content').append('<h5>Correct: ' + correctAnswers + '</h5>');
+	$('#content').append('<h5>Incorrect: ' + wrongAnswers + '</h5>');
+	$('#content').append('<h5>Total Score: ' + totalScore + '</h5>');
+	$('#content').append('<h4 id="reset-game">PLAY AGAIN</h4>');
 }
 
-
-function stop() {
-  //  Clears our intervalId
-  //  We just pass the name of the interval
-  //  to the clearInterval function.
-  clearInterval(intervalId);
-
-  if (number === 0) {
-    number = 30;
-  }
-
-}
-
-
-$('#start-game').click(questionOne);
-
-
-function questionOne() {
-	run();
-	$('.temp').remove();
+// Displays the Questions to the User
+function questionDisplay() {
+	$('#content').html('');
 	$('#time-bomb').append('<span id="timer"></span>');
-	$('#content').append('<h3 class="temp">What Movie Launched The Marvel Cinematic Universe?</h3>');
-	$('#content').append('<h4 class="temp" id="wrong1">THE INCREDIBLE HULK</h4>');
-	$('#content').append('<h4 class="temp" id="correct">IRONMAN</h4>');
-	$('#content').append('<h4 class="temp" id="wrong2">THOR</h4>');
-	$('#content').append('<h4 class="temp" id="wrong3">X-MEN</h4>');
+	$('#content').append('<h3 class="question">' + questions[i] + '</h3>');
+	$('#content').append('<h4 class="answer">' + answerArray[i][0] + '</h4>');
+	$('#content').append('<h4 class="answer">' + answerArray[i][1] + '</h4>');
+	$('#content').append('<h4 class="answer">' + answerArray[i][2] + '</h4>');
+	$('#content').append('<h4 class="answer">' + answerArray[i][3] + '</h4>');
+}
 
-	$('#wrong1,#wrong2,#wrong3').click(answerOneWrong);
-	$('#correct').click(answerOneCorrect);
+// Displays if User Guesses Correctly
+function correctDisplay() {
+	correctAnswers++;
+	$('#content').html('');
+	$('#content').append('<h3 class="temp">CORRECT! ' + correctArray[i] + '</h3>');
+	$('#content').append('<iframe src=' + gifArray[i] + '></iframe>');
+	setTimeout(nextDisplay, 4000);
+}
 
-	if (number === 0) {
-		answerOneTimedOut();
+// Displays if User Guesses Incorrectly
+function wrongDisplay() {
+	wrongAnswers++;
+	$('#content').html('');
+	$('#content').append('<h3 class="temp">WRONG! ' + correctArray[i] + '</h3>');
+	$('#content').append('<iframe src=' + gifArray[i] + '></iframe>');
+	setTimeout(nextDisplay, 4000);
+}
+
+// Displays if User Runs Out of Time
+function timedOutDisplay() {
+	timedOut++;
+	$('#content').html('');
+	$('#content').append('<h3 class="temp">TIMES UP! ' + correctArray[i] + '</h3>');
+	$('#content').append('<iframe src=' + gifArray[i] + '></iframe>');
+	setTimeout(nextDisplay, 4000);
+}
+
+// Brings on the Next Question Display and Resets Counter Time 
+function nextDisplay() {
+	if (i < 9) {
+		i++;
+		questionDisplay();
+		countdown();
+		counter = 30;
+	} else {
+		endScreen();
 	}
 
-
 }
 
-function answerOneCorrect() {
-	correctAnswers++;
-	$('.temp').remove();
-	stop();
-	$('#content').append('<h3 class="temp">CORRECT! IRONMAN</h3>');
-	$('#content').append('<iframe src="https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="290" frameBorder="0" class="giphy-embed"></iframe>');
-
+// Timer Function For Each Question (30sec)
+function countdown() {
+	intervalId = setInterval(decrement, 1000);
+	function decrement() {
+	counter--;
+	$("#timer").html("<h2>-" + counter + "</h2>");
+		if (counter === 0) {
+			clearInterval(intervalId);
+			timedOutDisplay();
+		}
+	}
 }
 
-function answerOneWrong() {
-	wrongAnswers++;
-	$('.temp').remove();
-	stop();
-	$('#content').append('<h3 class="temp">WRONG! IRONMAN</h3>');
-	$('#content').append('<iframe src="https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="366" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
-
+// Resets the Game Variables and Returns User to Start Screen
+function resetGame() {
+    i = 0;
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    timedOut = 0;
+    counter = 30;
+    startScreen();
 }
-
-function answerOneTimedOut() {
-	timedOut++;
-	$('.temp').remove();
-	stop();
-	$('#content').append('<h3 class="temp">YOU TOOK TOO LONG! IRONMAN</h3>');
-	$('#content').append('<iframe src="https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="366" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
-
-}
-
-
-
-
-
-
-
-
 
 
 
