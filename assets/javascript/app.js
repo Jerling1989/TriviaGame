d81bb595767dba67fd17c0fd328bcb0b7679a7cf
Jacob <jerling1989@gmail.com>
2018-02-06 19:18:12 -0500

    replace JS file path

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 var questions = ['What Movie Launched The Marvel Cinematic Universe?', 'What is the Name of Thor&rsquo;s Brother?', 'Who is the First Actor to Play Spiderman in the MCU?', 'What is Peter Quill&rsquo;s Self-Appointed Nickname?', 'What Was Ant-Man&rsquo;s Previous Career?', 'How Many Infinity Stones are in the MCU?', 'What is the Winter Soldier&rsquo;s Name?', 'What are the Only Words Groot Said Besides <q>I Am Groot</q>?', 'What Actor Played Doctor Strange?', 'What Phase is the MCU Currently In?'];
 var answerArray = [['THE INCREDIBLE HULK', 'IRONMAN', 'THOR', 'X-MEN'], ['ZEUS', 'APOLLO', 'JEFF', 'LOKI'], ['TOM HOLLAND', 'ANDREW GARFIELD', 'TOBEY MAGUIRE', 'NEIL PATRICK HARRIS'], ['MOON-GOD', 'SPACE-JESUS', 'STAR-LORD', 'LORD-BYRON'], ['CON-ARTIST', 'JEWELER', 'PLUMBER', 'BURGLAR'], ['SIX', 'THREE', 'FIVE', 'TEN'], ['FRANK WATTS', 'BUCKY BARNES', 'FATTY ARBUCKLE', 'NICKY BARNES'], ['WE ARE GROOT', 'I AM HUNGRY', 'I LOVE YOU', 'I LIKE TURTLES'], ['BUTTERCUP PUMPKINPATCH', 'BUFFALO CUSTARDBATH', 'BENEDICT CUMBERBATCH', 'BENEDRYL CUMBERSNATCH'], ['PHASE ONE', 'PHASE TWO', 'PHASE THREE', 'PHASE FOUR']];
 var correctArray = ['IRONMAN', 'LOKI', 'TOM HOLLAND', 'STAR-LORD', 'BURGLAR', 'SIX', 'BUCKY BARNES', 'WE ARE GROOT', 'BENEDICT CUMBERBATCH', 'PHASE THREE'];
-var gifArray = ['"https://giphy.com/embed/qmfpjpAT2fJRK" width="480" height="300" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/KiaAwaemKWhVu" width="480" height="259" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/I2LasqroRY8Mg" width="480" height="270" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/QgCwRigEAxqGQ" width="480" height="294" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/h0gzUb0Wh1RIY" width="480" height="300" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/aqMY57vLdkghi" width="480" height="198" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/oyyXEHFEZPLCo" width="480" height="274" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/F9hQLAVhWnL56" width="480" height="300" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/dvWvnRdc4h440" width="480" height="270" frameBorder="0" class="giphy-embed"', '"https://giphy.com/embed/RjSRwCJRhRB9S" width="480" height="300" frameBorder="0" class="giphy-embed"'];
+var gifArray = ['ironman', 'loki', 'tom-holland', 'star-lord', 'burglar', 'six', 'bucky-barnes', 'we-are-groot', 'benedict-cumberbacth', 'phase-three'];
 
 // VARIABLES FOR SCORE COUNT
 var totalScore = 0;
@@ -86,7 +86,7 @@ function correctDisplay() {
 	totalScore = correctAnswers * 10;
 	$('#content').html('');
 	$('#content').append('<h3 class="temp">CORRECT! ' + correctArray[i] + '</h3>');
-	$('#content').append('<iframe src=' + gifArray[i] + '></iframe>');
+	$('#content').append('<img class="answer-img" src="assets/images/gif/' + gifArray[i] + '.gif" />');
 	setTimeout(nextDisplay, 4000);
 }
 
@@ -95,7 +95,7 @@ function wrongDisplay() {
 	wrongAnswers++;
 	$('#content').html('');
 	$('#content').append('<h3 class="temp">WRONG! ' + correctArray[i] + '</h3>');
-	$('#content').append('<iframe src=' + gifArray[i] + '></iframe>');
+	$('#content').append('<img class="answer-img" src="assets/images/gif/' + gifArray[i] + '.gif" />');
 	setTimeout(nextDisplay, 4000);
 }
 
@@ -104,7 +104,7 @@ function timedOutDisplay() {
 	wrongAnswers++;
 	$('#content').html('');
 	$('#content').append('<h3 class="temp">TIMES UP! ' + correctArray[i] + '</h3>');
-	$('#content').append('<iframe src=' + gifArray[i] + '></iframe>');
+	$('#content').append('<img class="answer-img" src="assets/images/gif/' + gifArray[i] + '.gif" />');
 	setTimeout(nextDisplay, 4000);
 }
 
